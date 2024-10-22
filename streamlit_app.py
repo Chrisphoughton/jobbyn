1f7aa79d8c69650a1420a31635b359bf55b4472a
Christopher Houghton <chris@joinwda.com>
2024-10-22 19:40:21 -0400

    More tests

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.set_page_config(
+    page_title="WDAi Terminal",
+    page_icon="📈",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+        
+    }
+)
+
 st.title("Test Streamlit App")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
