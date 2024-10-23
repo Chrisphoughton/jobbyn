3fb3c41a0051bfb42d2d0e102dc895521e6bf7a2
Christopher Houghton <chris@joinwda.com>
2024-10-22 20:22:29 -0400

    basic setup

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,34 @@ st.set_page_config(
     }
 )
 
-st.title("Test Streamlit App")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+st.title("Jobbyn")
+st.subheader(
+    "Please follow instructions to get the best job for you"
 )
+st.divider()
+
+#read parquet file
+df = pd.read_parquet("data/jobs.parquet")
+#get list of job titles
+jobFamilies = df["jobFamily"].unique()
+
+
+jobOptions = st.multiselect(
+    "Select your Job Families",
+    jobFamilies,
+)
+locationOptions = st.multiselect(
+    "Select your Locations",
+    df["location"].unique(),
+)
+
+c1, c2 = st.columns([4, 1])
+with c2:
+    searchButton = st.button("Search for Jobs")
+st.divider()
+if searchButton:
+    df_options = df[df["jobFamily"].isin(jobOptions) & df["location"].isin(locationOptions)]
+    st.dataframe(df_options, use_container_width =True, hide_index=True)
+
+
