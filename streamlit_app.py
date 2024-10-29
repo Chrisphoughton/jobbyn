12b4e65030eb5de2369857a13f80e16da26902e3
Christopher Houghton <chris@joinwda.com>
2024-10-29 09:28:30 -0400

    Added search and skills

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import numpy as np
 from google.cloud import bigquery
 import altair as alt
 
-
 # Initialize the BigQuery client with the service account
 # service_account_info = "jobbyn-a159f5cec022.json"
 # client = bigquery.Client.from_service_account_json(service_account_info)
@@ -20,20 +19,21 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
     menu_items={
         'About': "# This is a header. This is an *extremely* cool app!"
-        
     }
 )
+
+#initialize session state
+if "df_options" not in st.session_state:
+    st.session_state.df_options = None
 
 # Function to query BigQuery and cache the result
 @st.cache_data
 def loading_lookups(query):
-    # Run the query and load results into a DataFrame
     query_job = client.query(query)
     dataframe = query_job.result().to_dataframe()
     return dataframe
 
 def loading_data(query):
-    # Run the query and load results into a DataFrame
     query_job = client.query(query)
     dataframe = query_job.result().to_dataframe()
     return dataframe
@@ -46,91 +46,107 @@ lookups_query = """
 # Use the cached function to get data
 df_lookups = loading_lookups(lookups_query)
 
-
-
-
 st.title("Jobbyn")
-
 st.divider()
 
-#get unique list of metro_area and sort them
+# Get unique list of metro_area and sort them
 locations = df_lookups["metro_area"].unique()
 locations.sort()
-#make location proper case
 locations = [loc.title() for loc in locations]
-locationOptions = st.multiselect(
-    "Select your Locations",
-    locations,
-)
-#convert locations to lower case
+locationOptions = st.multiselect("Select your Locations", locations)
 locationOptions = [loc.lower() for loc in locationOptions]
 
-#filter the data based on the selected locations
+# Filter the data based on the selected locations
 df_lookups = df_lookups[df_lookups["metro_area"].isin(locationOptions)]
-#get unique list of job families and sort them
 jobFamilies = df_lookups["mapped_role"].unique()
 jobFamilies.sort()
-#make job families proper case
 jobFamilies = [job.title() for job in jobFamilies]
-
-jobOptions = st.multiselect(
-    "Select your Job Families",
-    jobFamilies,
-)
-#convert job families to lower case
+jobOptions = st.multiselect("Select your Job Families", jobFamilies)
 jobOptions = [job.lower() for job in jobOptions]
 
-# Format the lists as strings for SQL IN clause
 formatted_location_options = ', '.join(f"'{loc}'" for loc in locationOptions) if locationOptions else "''"
 formatted_job_options = ', '.join(f"'{job}'" for job in jobOptions) if jobOptions else "''"
 
-#query where location and job family are selected
 data_query = f"""
     SELECT * FROM `jobbyn.jobbyn.jobs`
     WHERE metro_area IN ({formatted_location_options}) AND mapped_role IN ({formatted_job_options})
-   
 """
-
-
 
 c1, c2 = st.columns([1, 5])
 with c1:
     searchButton = st.button("Search for Jobs")
+
 st.divider()
+
 if searchButton:
     df_options = loading_data(data_query)
-    #drop job_id column
-    df_options = df_options.drop(columns=["job_id"])
-    #title case all column values
-   
-    #get the count of companies using group by
-    company_count = df_options.groupby("ultimate_parent_company_name").size().reset_index(name="Count")
-    #rename ultimate_parent_company_name to compnay
-    company_count.rename(columns={"ultimate_parent_company_name":"Company"}, inplace=True)
+    df_options = df_options
+    st.session_state["df_options"] = df_options  # Store DataFrame in session state
+
+if st.session_state["df_options"] is not None:
     
+    df_options = st.session_state["df_options"]
+    company_count = df_options.groupby("ultimate_parent_company_name").size().reset_index(name="Count")
+    company_count.rename(columns={"ultimate_parent_company_name": "Company"}, inplace=True)
+
     chart = alt.Chart(company_count).mark_bar().encode(
         x=alt.X("Count:Q", title="Number of Jobs"),
         y=alt.Y("Company:N", title="Company", sort="-x"),
-        
         tooltip=["Company", "Count"]
     )
-    st.altair_chart(chart, use_container_width=True)
-    st.dataframe(df_options, 
-                 column_config={
-                     "jobtitle_raw":st.column_config.TextColumn("Job Title"),
-                        "mapped_role":st.column_config.TextColumn("Job Family"),
-                        "ultimate_parent_company_name":st.column_config.TextColumn("Company"),
-                        "metro_area":st.column_config.TextColumn("Metro Area"),
-                        "state":st.column_config.TextColumn("State"),
-                        "location":st.column_config.TextColumn("Location"),
-                        "rics_k50":st.column_config.TextColumn("Industry"),
-                        "seniority":st.column_config.TextColumn("Seniority"),
-                        "total_compensation":st.column_config.NumberColumn("Total Compensation"),
-                        "remote_type":st.column_config.TextColumn("Remote Type"),
-                        "post_date":st.column_config.TextColumn("Posted Date"),
-                        
-                    },
-                 use_container_width =True, 
-                 hide_index=True)
+    with st.expander("Company Count", expanded=False):
+        st.altair_chart(chart, use_container_width=True)
+    #get dataframe from session state
 
+    event = st.dataframe(
+        df_options,
+        column_config={
+            "jobtitle_raw": st.column_config.TextColumn("Job Title"),
+            "mapped_role": st.column_config.TextColumn("Job Family"),
+            "ultimate_parent_company_name": st.column_config.TextColumn("Company"),
+            "metro_area": st.column_config.TextColumn("Metro Area"),
+            "state": st.column_config.TextColumn("State"),
+            "location": st.column_config.TextColumn("Location"),
+            "rics_k50": st.column_config.TextColumn("Industry"),
+            "seniority": st.column_config.TextColumn("Seniority"),
+            "total_compensation": st.column_config.NumberColumn("Total Compensation"),
+            "remote_type": st.column_config.TextColumn("Remote Type"),
+            "post_date": st.column_config.TextColumn("Posted Date"),
+            "job_id": st.column_config.TextColumn("Job ID"),
+        },
+        use_container_width=True,
+        selection_mode="single-row",
+        on_select="rerun",
+        hide_index=True
+    )
+    if event["selection"]["rows"]:
+        
+        row = event["selection"]["rows"][0]
+        #filter df_options based on the selected row
+        if row is not None:
+            st.divider()
+            st.write(df_options[df_options.index == row]["jobtitle_raw"].values[0])
+            selected_job_id = df_options[df_options.index == row]["job_id"].values[0]
+            skills_query = f"""
+                SELECT skill, mapped_skill, skill_k25, skill_k150, skill_k500, skill_k1000 FROM `jobbyn.jobbyn.skills`
+                WHERE job_id = {selected_job_id}
+            """
+            skills = loading_data(skills_query)
+            st.dataframe(
+                skills,
+                column_config={
+                    "skill": st.column_config.TextColumn("Original Skill"),
+                    "mapped_skill": st.column_config.TextColumn("Mapped Skill"),
+                    "skill_k25": st.column_config.TextColumn("Skill Group 1"),
+                    "skill_k150": st.column_config.TextColumn("Skill Group 2"),
+                    "skill_k500": st.column_config.TextColumn("Skill Group 3"),
+                    "skill_k1000": st.column_config.TextColumn("Skill Group 4"),
+                },
+                use_container_width=True,
+                hide_index=True
+            )
+            st.link_button("Search for Job", #use job title, company and metro area to search for job
+                           f"https://www.google.com/search?q={df_options[df_options.index == row]['jobtitle_raw'].values[0]} {df_options[df_options.index == row]['ultimate_parent_company_name'].values[0]} {df_options[df_options.index == row]['metro_area'].values[0]}")
+    else:
+        st.caption("Select a row to show skills")
 
