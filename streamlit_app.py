187a6ccbd7eabbddbdd7957855ad537221b1b3d8
Christopher Houghton <chris@joinwda.com>
2024-11-13 23:19:58 -0500

    Updated filters and loook and feel

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,18 @@ from google.cloud import bigquery
 import altair as alt
 
 # Initialize the BigQuery client with the service account
-# service_account_info = "jobbyn-a159f5cec022.json"
-# client = bigquery.Client.from_service_account_json(service_account_info)
+service_account_info = "jobbyn-a159f5cec022.json"
+client = bigquery.Client.from_service_account_json(service_account_info)
 
 # # Initialize the BigQuery client with the service account
-service_account_info = st.secrets["gcp_service_account"]
-client = bigquery.Client.from_service_account_info(service_account_info)
+# service_account_info = st.secrets["gcp_service_account"]
+# client = bigquery.Client.from_service_account_info(service_account_info)
 
 st.set_page_config(
     page_title="Jobbyn",
     page_icon="🏢",
     layout="wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="expanded",
     menu_items={
         'About': "# This is a header. This is an *extremely* cool app!"
     }
@@ -40,43 +40,91 @@ def loading_data(query):
 
 # Your SQL query
 lookups_query = """
-    SELECT DISTINCT mapped_role, rics_k50, metro_area FROM `jobbyn.jobbyn.jobs`
+    SELECT DISTINCT mapped_role, rics_k50, metro_area, seniority FROM `jobbyn.jobbyn.jobs`
 """
 
 # Use the cached function to get data
 df_lookups = loading_lookups(lookups_query)
+#make seniority a string
+df_lookups["seniority"] = df_lookups["seniority"].astype(str)
+
 
 st.title("Jobbyn")
+st.caption("A job search tool for the Jobbyn Method. Please select search filters on sidebar.")
 st.divider()
 
 # Get unique list of metro_area and sort them
-locations = df_lookups["metro_area"].unique()
-locations.sort()
-locations = [loc.title() for loc in locations]
-locationOptions = st.multiselect("Select your Locations", locations)
-locationOptions = [loc.lower() for loc in locationOptions]
+with st.sidebar:
+    locations = df_lookups["metro_area"].unique()
+    locations.sort()
+    locationsAllFlag = st.checkbox("Select All Locations")
+    if locationsAllFlag:
+        locationOptions = locations
+        st.multiselect("Select your Locations", locations,disabled=True,placeholder="All Locations Selected")
+    else:
+        locations = [loc.title() for loc in locations]
+        locationOptions = st.multiselect("Select your Locations", locations)
+        locationOptions = [loc.lower() for loc in locationOptions]
+    st.divider()
 
-# Filter the data based on the selected locations
-df_lookups = df_lookups[df_lookups["metro_area"].isin(locationOptions)]
-jobFamilies = df_lookups["mapped_role"].unique()
-jobFamilies.sort()
-jobFamilies = [job.title() for job in jobFamilies]
-jobOptions = st.multiselect("Select your Job Families", jobFamilies)
-jobOptions = [job.lower() for job in jobOptions]
+    #filter industries based on selected locations
+    df_lookups = df_lookups[df_lookups["metro_area"].isin(locationOptions)]
+    industries = df_lookups["rics_k50"].unique()
+    #remove null values
+    industries = [industry for industry in industries if industry is not None]
+    industries.sort()
+    industriesAllFlag = st.checkbox("Select All Industries")
+    if industriesAllFlag:
+        industryOptions = industries
+        st.multiselect("Select your Industries", industries,disabled=True,placeholder="All Industries Selected")
+    else:
+        industryOptions = st.multiselect("Select your Industries", industries)
+    st.divider()
 
-formatted_location_options = ', '.join(f"'{loc}'" for loc in locationOptions) if locationOptions else "''"
-formatted_job_options = ', '.join(f"'{job}'" for job in jobOptions) if jobOptions else "''"
+    #filter job families based on selected locations
+    df_lookups = df_lookups[df_lookups["rics_k50"].isin(industryOptions)]
+    jobFamilies = df_lookups["mapped_role"].unique()
+    jobFamilies.sort()
+    jobFamiliesAllFlag = st.checkbox("Select All Job Families")
+    if jobFamiliesAllFlag:
+        jobOptions = jobFamilies
+        st.multiselect("Select your Job Families", jobFamilies,disabled=True,placeholder="All Job Families Selected")
+    else:
+        jobFamilies = [job.title() for job in jobFamilies]
+        jobOptions = st.multiselect("Select your Job Families", jobFamilies)
+        jobOptions = [job.lower() for job in jobOptions]
+    st.divider()
 
-data_query = f"""
-    SELECT * FROM `jobbyn.jobbyn.jobs`
-    WHERE metro_area IN ({formatted_location_options}) AND mapped_role IN ({formatted_job_options})
-"""
+    #filter seniority based on selected job families and locations
+    df_lookups = df_lookups[df_lookups["mapped_role"].isin(jobOptions)]
+    seniority = df_lookups["seniority"].unique()
+    seniority.sort()
+    seniorityAllFlag = st.checkbox("Select All Seniority Levels")
+    if seniorityAllFlag:
+        seniorityOptions = seniority
+        st.multiselect("Select your Seniority Levels", seniority,disabled=True,placeholder="All Seniority Levels Selected")
+    else:
+        seniorityOptions = st.multiselect("Select your Seniority Levels", seniority)
+    st.divider()
 
-c1, c2 = st.columns([1, 5])
-with c1:
-    searchButton = st.button("Search for Jobs")
+    formatted_location_options = ', '.join(f"'{loc}'" for loc in locationOptions) if len(locationOptions)>0 else "''"
+    formatted_job_options = ', '.join(f"'{job}'" for job in jobOptions) if len(jobOptions)>0 else "''"
+    formatted_seniority_options = ', '.join(f"'{seniority}'" for seniority in seniorityOptions) if len(seniorityOptions)>0 else "''"
+    formatted_industry_options = ', '.join(f"'{industry}'" for industry in industryOptions) if len(industryOptions)>0 else "''"
 
-st.divider()
+    data_query = f"""
+        SELECT * FROM `jobbyn.jobbyn.jobs`
+        WHERE metro_area IN ({formatted_location_options}) AND mapped_role IN ({formatted_job_options}) AND seniority IN ({formatted_seniority_options}) AND rics_k50 IN ({formatted_industry_options})
+    """
+
+    searchButton = st.button("Search for Jobs",disabled=True if (len(locationOptions) == 0 or len(jobOptions) == 0 or len(seniorityOptions) == 0 or len(industryOptions) == 0 or len(df_lookups) > 100000) else False)
+    #if either of the options are empty, return an empty dataframe
+    if len(df_lookups) > 100000:
+        st.caption("Too many options selected, please narrow your search")
+    if len(locationOptions) == 0 or len(jobOptions) == 0 or len(seniorityOptions) == 0 or len(industryOptions) == 0:
+        st.caption("Please select at least one option for each category")
+    
+
 
 if searchButton:
     df_options = loading_data(data_query)
