89aeac249c31a7cc924cb4757685db093012a72e
Christopher Houghton <chris@joinwda.com>
2024-11-24 12:09:10 -0500

    New version with skills and responsibilities and job search

---
+++ example Experiences.md
@@ -0,0 +1,30 @@
+Operations Manager|FRG Technologies
+Milwaukee, WI - December 2016 - Current
+- Direct day-to-day operations focused on the attainment of key business metrics, continuous improvement initiatives and an eight-member team with related direct reports.
+- Amplify organizational quality standards by delivering 18% increased quality during a three-year timeframe.
+- Maximize productivity and management systems by establishing specific goals and managing operations.
+- Plan, organize and direct distribution operations to ensure optimal return on investment for the company.
+
+Assistant Operations Manager|Addison Group
+Milwaukee, WI - June 2014 - December 2016
+- Accelerated efficiency of operations by assisting with controlling budgets, overseeing customer accounts, managing scheduling and driving meetings.
+- Maintained up-to-date on business operations as well as positive and negative impacts, improving productivity by 45%.
+- Optimized organizational operations and facilitated decision-making by examining problem-solving concepts, including quantitative methods and techniques.
+
+Operations Associate|GitHub
+Milwaukee, WI - September 2011 - May 2014
+- Managed daily office operations, including 25 client accounts, supply inventory and record management.
+- Maintained daily reports and advised executive leaders in decision-making processes.
+- Developed recordkeeping systems for 100 employees' records and company documents to optimize operations and reduce project lags.
+
+
+UNITED STATES AIR FORCE (deployments around the globe) 5/2014 to 5/2022
+Aviation Maintenance Supervisor
+
+- Led a dynamic team of avionics specialists to maintain and repair hundreds of millions of dollars worth of critical aircraft assets. Conducted regular scheduled maintenance inspections, identified needed repairs, and worked with the team to ensure the highest levels of operational effectiveness and safety.
+
+- Trained and mentored team members to ensure that all relevant safety standards were maintained at all times. Supervised usage of personal protective equipment and monitored compliance with HazMat regulations.
+
+- Scheduled and prioritized aircraft maintenance and repair to meet the goal of ensuring operational flights exceeding 4,000 each year. Maintained a 97% success rate for timely maintenance completion.
+
+- Focus on attention to detail and effective planning resulted in a 100% quality assurance rate over a five-year period, resulting in more than 98% flight readiness.
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from google.cloud import bigquery
 import altair as alt
+from openai import OpenAI
 
 # Initialize the BigQuery client with the service account
 # service_account_info = "jobbyn-a159f5cec022.json"
@@ -11,6 +12,11 @@ import altair as alt
 # # Initialize the BigQuery client with the service account
 service_account_info = st.secrets["gcp_service_account"]
 client = bigquery.Client.from_service_account_info(service_account_info)
+
+# openAiClient = OpenAI(api_key='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')
+
+openAi_info = st.secrets["openai"]["api_key"]
+openAiClient = OpenAI(api_key=openAi_info)
 
 st.set_page_config(
     page_title="Jobbyn",
@@ -26,21 +32,63 @@ st.set_page_config(
 if "df_options" not in st.session_state:
     st.session_state.df_options = None
 
+if "user_experience" not in st.session_state:
+    st.session_state.user_experience = None
+
+if "job_skills" not in st.session_state:
+    st.session_state.job_skills = None
+
+if "job_responsibilities" not in st.session_state:
+    st.session_state.job_responsibilities = None
+
+seniority_map = { "1": "1. Entry Level", "2": "2. Junior Level", "3": "3. Associate Level", "4": "4. Manager Level ", "5": "5. Director Level", "6": "6. Executive Level", "7": "7. Senior Executive Level "}
+
 # Function to query BigQuery and cache the result
 @st.cache_data
 def loading_lookups(query):
     query_job = client.query(query)
     dataframe = query_job.result().to_dataframe()
+    #make senorty a string
+    dataframe["seniority"] = dataframe["seniority"].astype(str)
+    dataframe["seniority"] = dataframe["seniority"].map(seniority_map)
+    #make mapped_role title case
+    
     return dataframe
 
 def loading_data(query):
     query_job = client.query(query)
     dataframe = query_job.result().to_dataframe()
+    #append https:// if posting_url does not have it
+    dataframe["posting_url"] = dataframe["posting_url"].apply(lambda x: x if x.startswith("http") else "https://www." + x)
+    dataframe["seniority"] = dataframe["seniority"].map(seniority_map)
+    dataframe["mapped_role"] = dataframe["mapped_role"].str.title()
+    dataframe["metro_area"] = dataframe["metro_area"].str.title()
+    #reorder columsn to have posting_url after job_id
+    dataframe = dataframe[['job_id','jobtitle_raw','posting_url', 'mapped_role', 'ultimate_parent_company_name', 'metro_area', 'state', 'location', 'rics_k50', 'seniority', 'total_compensation', 'remote_type', 'post_date']]
+   
+    return dataframe
+
+def loading_skill(query):
+    query_job = client.query(query)
+    dataframe = query_job.result().to_dataframe()
+    #set "" to None
+    dataframe["skills"] = dataframe["skills"].apply(lambda x: None if x == "" else x)
+    
+   
+    return dataframe
+
+def loading_responsibilities(query):
+    query_job = client.query(query)
+    dataframe = query_job.result().to_dataframe()
+    #set "" to None
+    dataframe["responsibilities"] = dataframe["responsibilities"].apply(lambda x: None if x == "" else x)
+    
+   
     return dataframe
 
 # Your SQL query
 lookups_query = """
-    SELECT DISTINCT mapped_role, rics_k50, metro_area, seniority FROM `jobbyn.jobbyn.jobs`
+    SELECT DISTINCT mapped_role, rics_k50, metro_area, seniority FROM `jobbyn.jobbyn.jobsUpdated`
 """
 
 # Use the cached function to get data
@@ -49,9 +97,8 @@ df_lookups = loading_lookups(lookups_query)
 df_lookups["seniority"] = df_lookups["seniority"].astype(str)
 
 
-st.title("Jobbyn")
-st.caption("A job search tool for the Jobbyn Method. Please select search filters on sidebar.")
-st.divider()
+st.image("jobbynLogo.jpg", width = 340)
+
 
 # Get unique list of metro_area and sort them
 with st.sidebar:
@@ -105,6 +152,9 @@ with st.sidebar:
         st.multiselect("Select your Seniority Levels", seniority,disabled=True,placeholder="All Seniority Levels Selected")
     else:
         seniorityOptions = st.multiselect("Select your Seniority Levels", seniority)
+    #for seniority options take the first leftmost digit and convert to number
+    seniorityOptions = [seniority[0] for seniority in seniorityOptions]
+
     st.divider()
 
     formatted_location_options = ', '.join(f"'{loc}'" for loc in locationOptions) if len(locationOptions)>0 else "''"
@@ -113,7 +163,7 @@ with st.sidebar:
     formatted_industry_options = ', '.join(f"'{industry}'" for industry in industryOptions) if len(industryOptions)>0 else "''"
 
     data_query = f"""
-        SELECT * FROM `jobbyn.jobbyn.jobs`
+        SELECT * FROM `jobbyn.jobbyn.jobsUpdated`
         WHERE metro_area IN ({formatted_location_options}) AND mapped_role IN ({formatted_job_options}) AND seniority IN ({formatted_seniority_options}) AND rics_k50 IN ({formatted_industry_options})
     """
 
@@ -125,76 +175,173 @@ with st.sidebar:
         st.caption("Please select at least one option for each category")
     
 
+t1, t2 = st.tabs(["Job Search", "Your Experiences"])
+with t1:
+    if searchButton:
+        df_options = loading_data(data_query)
+        df_options = df_options
+        st.session_state["df_options"] = df_options  # Store DataFrame in session state
 
-if searchButton:
-    df_options = loading_data(data_query)
-    df_options = df_options
-    st.session_state["df_options"] = df_options  # Store DataFrame in session state
-
-if st.session_state["df_options"] is not None:
+    if st.session_state["df_options"] is not None:
     
-    df_options = st.session_state["df_options"]
-    company_count = df_options.groupby("ultimate_parent_company_name").size().reset_index(name="Count")
-    company_count.rename(columns={"ultimate_parent_company_name": "Company"}, inplace=True)
+    
+        df_options = st.session_state["df_options"]
+        company_count = df_options.groupby("ultimate_parent_company_name").size().reset_index(name="Count")
+        company_count.rename(columns={"ultimate_parent_company_name": "Company"}, inplace=True)
 
-    chart = alt.Chart(company_count).mark_bar().encode(
-        x=alt.X("Count:Q", title="Number of Jobs"),
-        y=alt.Y("Company:N", title="Company", sort="-x"),
-        tooltip=["Company", "Count"]
-    )
-    with st.expander("Company Count", expanded=False):
-        st.altair_chart(chart, use_container_width=True)
-    #get dataframe from session state
+        role_count = df_options.groupby("mapped_role").size().reset_index(name="Count")
+        role_count.rename(columns={"mapped_role": "jobFamily"}, inplace=True)
 
-    event = st.dataframe(
-        df_options,
-        column_config={
-            "jobtitle_raw": st.column_config.TextColumn("Job Title"),
-            "mapped_role": st.column_config.TextColumn("Job Family"),
-            "ultimate_parent_company_name": st.column_config.TextColumn("Company"),
-            "metro_area": st.column_config.TextColumn("Metro Area"),
-            "state": st.column_config.TextColumn("State"),
-            "location": st.column_config.TextColumn("Location"),
-            "rics_k50": st.column_config.TextColumn("Industry"),
-            "seniority": st.column_config.TextColumn("Seniority"),
-            "total_compensation": st.column_config.NumberColumn("Total Compensation"),
-            "remote_type": st.column_config.TextColumn("Remote Type"),
-            "post_date": st.column_config.TextColumn("Posted Date"),
-            "job_id": st.column_config.TextColumn("Job ID"),
-        },
-        use_container_width=True,
-        selection_mode="single-row",
-        on_select="rerun",
-        hide_index=True
-    )
-    if event["selection"]["rows"]:
-        
-        row = event["selection"]["rows"][0]
-        #filter df_options based on the selected row
-        if row is not None:
-            st.divider()
-            st.write(df_options[df_options.index == row]["jobtitle_raw"].values[0])
-            selected_job_id = df_options[df_options.index == row]["job_id"].values[0]
-            skills_query = f"""
-                SELECT skill, mapped_skill, skill_k25, skill_k150, skill_k500, skill_k1000 FROM `jobbyn.jobbyn.skills`
-                WHERE job_id = {selected_job_id}
-            """
-            skills = loading_data(skills_query)
-            st.dataframe(
-                skills,
-                column_config={
-                    "skill": st.column_config.TextColumn("Original Skill"),
-                    "mapped_skill": st.column_config.TextColumn("Mapped Skill"),
-                    "skill_k25": st.column_config.TextColumn("Skill Group 1"),
-                    "skill_k150": st.column_config.TextColumn("Skill Group 2"),
-                    "skill_k500": st.column_config.TextColumn("Skill Group 3"),
-                    "skill_k1000": st.column_config.TextColumn("Skill Group 4"),
-                },
-                use_container_width=True,
-                hide_index=True
-            )
-            st.link_button("Search for Job", #use job title, company and metro area to search for job
-                           f"https://www.google.com/search?q={df_options[df_options.index == row]['jobtitle_raw'].values[0]} {df_options[df_options.index == row]['ultimate_parent_company_name'].values[0]} {df_options[df_options.index == row]['metro_area'].values[0]}")
-    else:
-        st.caption("Select a row to show skills")
+        chart = alt.Chart(company_count).mark_bar().encode(
+            x=alt.X("Count:Q", title="Number of Jobs"),
+            y=alt.Y("Company:N", title="Company", sort="-x"),
+            tooltip=["Company", "Count"],
+            color=alt.value("#004481")
+        ).configure_axis(
+            domain=False,
+            grid=False,
+            titleFontSize=12,
+            labelOverlap=True,
+            labelLimit=300,
+        ).properties(
+            width=700,
+            title="Company Count" 
+        )
+
+        chart2 = alt.Chart(role_count).mark_bar().encode(
+            x=alt.X("Count:Q", title="Number of Jobs"),
+            y=alt.Y("jobFamily:N", title="Job Family", sort="-x"),
+            tooltip=["jobFamily", "Count"],
+            color=alt.value("#004481")
+        ).configure_axis(
+            domain=False,
+            grid=False,
+            titleFontSize=12,
+            labelOverlap=True,
+            labelLimit=300,
+            
+        ).properties(
+            width=700,
+            title="Job Family" 
+        )
+        with st.expander("Summary", expanded=False):
+            c1, c2 = st.columns([1, 1])
+            with c1:
+                with st.container(height=600):
+                    st.altair_chart(chart, use_container_width=True)
+            
+            with c2:
+                with st.container(height=600):
+                    st.altair_chart(chart2, use_container_width=True)
+        #get dataframe from session state
+
+        event = st.dataframe(
+            df_options,
+            column_config={
+                "jobtitle_raw": st.column_config.TextColumn("Job Title"),
+                "mapped_role": st.column_config.TextColumn("Job Family"),
+                "ultimate_parent_company_name": st.column_config.TextColumn("Company"),
+                "metro_area": st.column_config.TextColumn("Metro Area"),
+                "state": st.column_config.TextColumn("State"),
+                "location": st.column_config.TextColumn("Location"),
+                "rics_k50": st.column_config.TextColumn("Industry"),
+                "seniority": st.column_config.TextColumn("Seniority"),
+                "total_compensation": st.column_config.NumberColumn("Total Compensation"),
+                "remote_type": st.column_config.TextColumn("Remote Type"),
+                "post_date": st.column_config.TextColumn("Posted Date"),
+                "job_id": st.column_config.TextColumn("Job ID"),
+                "posting_url": st.column_config.LinkColumn("Posting Link", display_text="🔗"),
+            },
+            use_container_width=True,
+            selection_mode="single-row",
+            on_select="rerun",
+            hide_index=True
+        )
+        if event["selection"]["rows"]:
+            
+            row = event["selection"]["rows"][0]
+            #filter df_options based on the selected row
+            if row is not None:
+                st.divider()
+                job_and_company = df_options[df_options.index == row]["jobtitle_raw"].values[0] + " at " + df_options[df_options.index == row]["ultimate_parent_company_name"].values[0]
+                st.write(job_and_company)
+                selected_job_id = df_options[df_options.index == row]["job_id"].values[0]
+                skills_query = f"""
+                    SELECT skills FROM `jobbyn.jobbyn.skill`
+                    WHERE job_id = {selected_job_id}
+                """
+                skills = loading_skill(skills_query)
+
+                responsibilities_query = f"""
+                    SELECT responsibilities FROM `jobbyn.jobbyn.responsibilities`
+                    WHERE job_id = {selected_job_id}
+                """
+                responsibilities = loading_responsibilities(responsibilities_query)
+
+                c1,c2 = st.columns([1,1])
+                with c1:
+                    with st.container(border=True):
+                        st.caption("Skills")
+                        st.session_state["job_skills"] = skills["skills"].values[0]
+                        st.write(st.session_state["job_skills"])
+                with c2:
+                    with st.container(border=True):
+                        st.caption("Responsibilities")
+                        st.session_state["job_responsibilities"] = responsibilities["responsibilities"].values[0]
+                        st.write(st.session_state["job_responsibilities"])
+                
+
+                st.divider()
+                if st.button("🤖 Tailor your Experience to the job"):
+                    if st.session_state["user_experience"]:
+                        # OpenAI API Prompt
+                        prompt = f"""
+
+                        Job title and company:
+                        {job_and_company}
+
+
+                        Job Posting Skills: 
+                        {st.session_state["job_skills"]}
+
+                        Job Posting Responsibilities:
+                        {st.session_state["job_responsibilities"]}
+
+
+                        User's Experience:
+                        {st.session_state["user_experience"]}
+
+                        Update the user's experience to better align with the job's skills and responsibilities. Maintain authenticity while highlighting relevant accomplishments and transferable skills. Ensure the tone is professional and tailored for a resume.
+                        """
+                        with st.container(border=True):
+                            with st.chat_message("ai"):
+                                try:
+                                    # Call OpenAI API
+                                    response = openAiClient.chat.completions.create(
+                                        model="gpt-4o",  # Or "text-davinci-003"
+                                        messages=[{"role": "system", "content": "You are a helpful assistant that is aiding the user in updating their work experience based on the job posting. They will provide you skills and their current experiences and you need to update their experiences to better align with the job posting."},
+                                                {"role": "user", "content": prompt}],
+                                        temperature=1,
+                                        max_tokens=500,
+                                        stream=True
+                                    )
+
+                                    # Display Updated Experience
+                                    st.write_stream( response)
+
+                                except Exception as e:
+                                    st.error(f"Error generating updated experience: {e}")
+                    else:
+                        st.warning("Please fill out your Your Experiences before generating.")
+
+
+                
+                
+        else:
+            st.caption("Select a row to show skills, responsibilites and to tailor your resume")
+    
+    with t2:
+      
+        user_experience = st.text_area("Enter your experiences here", height=500)
+        st.session_state["user_experience"] = user_experience
 
